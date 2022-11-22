667daaa79f891ea66294447197cf95daef61d490
bjoernhansen <hansen_bjoern@web.de>
2022-11-22 23:31:18 +0100

    turned into gradle project

--- src/main/java/training/Main.java
+++ src/main/java/training/Main.java
@@ -7,14 +7,16 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        root.getStylesheets().add("training/style.css");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/sample.fxml")));
+        root.getStylesheets().add("style.css");
         primaryStage.setTitle("Animal Name Writer");
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
         primaryStage.setScene(new Scene(root));
         primaryStage.sizeToScene();
         primaryStage.show();
